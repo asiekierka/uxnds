c8a0ffc4fb4bd06b043e24499467ae75d9d21fc4
neauoire <aliceffekt@gmail.com>
2021-01-29 11:35:59 -0800

    *

--- uxn.c
+++ uxn.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* 
+/*
 Copyright (c) 2021 Devine Lu Linvega
 
 Permission to use, copy, modify, and distribute this software for any
@@ -11,9 +11,28 @@ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 WITH REGARD TO THIS SOFTWARE.
 */
 
+typedef unsigned char Uint8;
+#define STACK_DEPTH 256
+
+Uint8 data[STACK_DEPTH];
+Uint8 address[STACK_DEPTH];
+
+void
+stackprint(Uint8 *stack)
+{
+	int i;
+	for(i = 0; i < STACK_DEPTH; ++i) {
+		if(i % 16 == 0)
+			printf("\n");
+		printf("%02x ", stack[i]);
+	}
+	printf("\n");
+}
+
 int
 main(int argc, char *argv[])
 {
 	printf("hello\n");
+	stackprint(data);
 	return 0;
 }
