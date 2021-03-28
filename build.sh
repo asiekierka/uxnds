47866bbe87760ff9fa8e1cccdbc4764ccd0338f4
neauoire <aliceffekt@gmail.com>
2021-03-28 11:12:00 -0700

    Ported the software to the new disk location

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ else
 fi
 
 echo "Assembling.."
-./bin/assembler projects/software/left.usm bin/boot.rom
+./bin/assembler projects/software/neralie.usm bin/boot.rom
 
 echo "Running.."
 if [ "${2}" = '--cli' ]; 
