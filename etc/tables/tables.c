a3ce59fa93d11453a12faa86fee58d68bd26af80
neauoire <aliceffekt@gmail.com>
2021-04-01 21:44:23 -0700

    Improved clock

--- etc/tables/tables.c
+++ etc/tables/tables.c
@@ -17,20 +17,26 @@ WITH REGARD TO THIS SOFTWARE.
 typedef unsigned char Uint8;
 
 int
+clamp(int val, int min, int max)
+{
+	return (val >= min) ? (val <= max) ? val : max : min;
+}
+
+int
 main()
 {
 	int i;
 	printf("60 points on a circle128(bytex,bytey):\n\n");
 	for(i = 0; i < 60; ++i) {
-		double cx = 128, cy = 128, r = 127;
-		double pos = (60 - i + 30) % 60;
+		double cx = 128, cy = 128, r = 128;
+		double pos = (i - 15) % 60;
 		double deg = (pos / 60.0) * 360.0;
 		double rad = deg * (PI / 180);
 		double x = cx + r * cos(rad);
 		double y = cy + r * sin(rad);
 		if(i > 0 && i % 8 == 0)
 			printf("\n");
-		printf("%02x%02x ", (Uint8)x, (Uint8)y);
+		printf("%02x%02x ", (Uint8)clamp(x, 0x00, 0xff), (Uint8)clamp(y, 0x00, 0xff));
 	}
 	printf("\n\n");
 	return 0;
