5334e5cfa054ded4c24a755231c687e96f2e85c6
neauoire <aliceffekt@gmail.com>
2021-04-08 22:00:06 -0700

    Progress on orca

--- src/apu.h
+++ src/apu.h
@@ -10,10 +10,6 @@ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 WITH REGARD TO THIS SOFTWARE.
 */
 
-typedef unsigned char Uint8;
-typedef signed char Sint8;
-typedef unsigned short Uint16;
-typedef signed short Sint16;
 typedef unsigned int Uint32;
 typedef signed int Sint32;
 
