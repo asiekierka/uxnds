f14f8a64f4122002318d3f94a6cbfd2da53de0fd
neauoire <aliceffekt@gmail.com>
2021-04-09 20:06:45 -0700

    Progress on orca

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ else
 fi
 
 echo "Assembling.."
-./bin/assembler projects/examples/dev.audio.usm bin/boot.rom
+./bin/assembler projects/software/nasu.usm bin/boot.rom
 
 echo "Running.."
 if [ "${2}" = '--cli' ]; 
