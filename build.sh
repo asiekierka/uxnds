c0b337e9330d0b576a3f2e6773e6dadb27b591b7
neauoire <aliceffekt@gmail.com>
2021-04-21 12:00:31 -0700

    ported dvd animation

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ else
 fi
 
 echo "Assembling.."
-./bin/assembler projects/examples/dev.file.usm bin/boot.rom
+./bin/assembler projects/examples/gui.animation.usm bin/boot.rom
 
 echo "Running.."
 if [ "${2}" = '--cli' ]; 
