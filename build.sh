0b5b6d077a119d8b64dcf7e460329fd086f30cb4
neauoire <aliceffekt@gmail.com>
2021-05-09 11:22:21 -0700

    Progress on Nasu

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ else
 fi
 
 echo "Assembling.."
-./bin/assembler projects/demos/drum-rack.usm bin/boot.rom
+./bin/assembler projects/software/nasu.usm bin/boot.rom
 
 echo "Running.."
 if [ "${2}" = '--cli' ]; 
